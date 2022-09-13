fd8fa6367284252b281b583900f3924f536c84c7
Dominik Mrówka <dmrowka7@gmail.com>
2022-09-13 20:05:23 +0200

    Add navbar and basic style

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import style from './NavBar.module.scss';
+import { Link, NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div className={style.navBar}>
+      <div className={style.container}>
+        <div className={style.iconLeft}>
+          <span className={style.main}><Link to="/">Notice Board</Link></span>
+        </div>
+        <div className={style.navigation}>
+          <ul className={style.ulNav}>
+            <li><NavLink className={({ isActive }) => isActive ? style.linkActive : undefined} to="/">Home</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? style.linkActive : undefined} to="/signin">Sign in</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? style.linkActive : undefined} to="/register">Register</NavLink></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
