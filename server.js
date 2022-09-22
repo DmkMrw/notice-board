dd4f2fe05b7df263e540cce1de40d898c8348af4
Dominik Mrówka <dmrowka7@gmail.com>
2022-09-22 18:39:28 +0200

    Add url to remote db

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const NODE_ENV = process.env.NODE_ENV;
 let dbUri = '';
 
-if (NODE_ENV === 'production') dbUri = 'url to remote db';
+if (NODE_ENV === 'production') dbUri = process.env.DB_URL;
 else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/adsDBtest';
 else dbUri = 'mongodb://localhost:27017/adsDB';
 
