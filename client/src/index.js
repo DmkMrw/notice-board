d87b7d130afed1f8d3089335d0b451ada0d7da2d
Dominik Mrówka <dmrowka7@gmail.com>
2022-09-23 15:14:28 +0200

    fix heroku

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import 'font-awesome/css/font-awesome.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+require('dotenv').config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
